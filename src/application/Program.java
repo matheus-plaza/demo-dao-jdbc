cbc9d7b216e760ddc741ead39930488921efbb36
Matheus Plaza <plazachave.mp7@gmail.com>
2025-04-05 16:40:37 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		obj.toString();
+		
+		Seller obj2 = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(obj2);
 
 	}
 
