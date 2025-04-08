ecc6f673554e0d8fabd02404d19af9366809077e
Matheus Plaza <plazachave.mp7@gmail.com>
2025-04-08 15:05:51 -0300

    Department deleted

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -90,7 +90,23 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void deleteById(Integer id) {
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"delete from department where id = ?");
+			
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				System.out.println("Department deleted!");
+			}
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 	}
 	
 	@Override
