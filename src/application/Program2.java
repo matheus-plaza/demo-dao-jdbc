785dec92eb4aee006be277458601c6209f8a3c4e
Matheus Plaza <plazachave.mp7@gmail.com>
2025-04-08 14:22:40 -0300

    Department update

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -11,13 +11,14 @@ public class Program2 {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("\n=== Teste 1: Department insert ===");
-		Department newDepartment = new Department(null, "Test");
+		Department newDepartment = new Department(null, "RH");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = "+ newDepartment.getId());
 
 		System.out.println("\n=== Teste 2: Department update ===");
+		newDepartment.setName("Test2");
 		departmentDao.update(newDepartment);
-		System.out.println("id = "+newDepartment.getId()+ " , name - " + newDepartment.getName());
+		System.out.println("id = " + newDepartment.getId()+ " , name - " + newDepartment.getName());
 		
 	}
 
